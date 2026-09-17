fca7809ae0191c296b09bf695f53cfefe574fa7f
Chetan Utture <utture.chetan@gmail.com>
2026-09-17 18:26:11 +0530

    update the expired key

--- backend.py
+++ backend.py
@@ -155,7 +155,7 @@ def get_stock_price(symbol: str) -> dict:
     Fetch latest stock price for a given symbol (e.g. 'AAPL', 'TSLA') 
     using Alpha Vantage with API key in the URL.
     """
-    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey=9MZO2JUBR7IFNTOI"
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey=OH16AM8EIKGI0R97"
     r = requests.get(url)
     return r.json()
 
